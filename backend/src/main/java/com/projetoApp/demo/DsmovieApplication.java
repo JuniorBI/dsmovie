373f33a524d36c8cde65291e8094ffa541068450
Junior-Antonio <antonio.silvajunior.as@gmail.com>
2022-03-08 15:09:25 -0300

    primeiro commit do projeto

---
+++ backend/src/main/java/com/projetoApp/demo/DsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.projetoApp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/projetoApp/demo/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoApp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
